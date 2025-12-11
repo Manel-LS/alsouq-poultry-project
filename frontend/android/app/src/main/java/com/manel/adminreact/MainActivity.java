399ee2f0274576a2bbb45c707c6eb39573f230af
Manel-LS <manel.leadersoft@gmail.com>
2025-12-11 17:17:14 +0100

    inital commit

--- frontend/android/app/src/main/java/com/manel/adminreact/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.manel.adminreact;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- frontend/src/views/pages/login/config.jsx
+++
@@ -1,2 +0,0 @@
-// Re-export central API config to keep backward compatibility
-export { API_CONFIG, API_ENDPOINTS, ERROR_MESSAGES, getDatabaseChoice, setDatabaseChoice, getAuthHeaders } from '../../../config/api.js'
